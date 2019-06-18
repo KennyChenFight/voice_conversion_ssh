84998d0610490a419731bdf406e48104428669f5
KennyChen <kennychen851228@gmail.com>
2019-06-18 14:51:24 +0800

    重構完畢~

--- ssh.py
+++ ssh.py
@@ -11,10 +11,25 @@ class SSH:
     password = 'voice123!'
     local_source_wav_dir_path = 'source_wav/'
     local_target_wav_dir_path = 'target_wav/'
-    server_source_wav_dir_path = '/home/voicetraining/merlin/egs/voice_conversion/s1/experiments/SF12TM1/test_synthesis/SF1'
-    server_target_wav_dir_path = '/home/voicetraining/merlin/egs/voice_conversion/s1/experiments/SF12TM1/test_synthesis/TM1'
-    convert_wav_command = 'cd merlin/egs/voice_conversion/s1/; bash /home/voicetraining/merlin/egs/voice_conversion/s1/run_final.sh SF1 TM1'
+    model_name = ''
+    server_source_wav_dir_path = '/home/voicetraining/merlin/egs/voice_conversion/s1/experiments/source2target/test_synthesis/source'
+    server_target_wav_dir_path = '/home/voicetraining/merlin/egs/voice_conversion/s1/experiments/source2target/test_synthesis/target'
+    convert_wav_command = 'cd merlin/egs/voice_conversion/s1/; bash /home/voicetraining/merlin/egs/voice_conversion/s1/run_final.sh source target'
+    show_model_list_command = '/home/voicetraining/merlin/egs/voice_conversion/s1/'
+    rename_model_to_experiments_command = 'cd merlin/egs/voice_conversion/s1/' + '; mv model experiments'
+    rename_experiments_to_model_command = 'cd merlin/egs/voice_conversion/s1/' + '; mv experiments model'
 
+    # 根據選擇的model動態變換路徑
+    @classmethod
+    def init_path(cls, model_name):
+        cls.model_name = model_name
+        old_experiments_name = cls.server_source_wav_dir_path.split('/')[-4]
+        cls.server_source_wav_dir_path = cls.server_source_wav_dir_path.replace(old_experiments_name, model_name)
+        cls.server_target_wav_dir_path = cls.server_target_wav_dir_path.replace(old_experiments_name, model_name)
+        old_model_name = cls.rename_model_to_experiments_command.split(' ')[-2]
+        cls.rename_model_to_experiments_command = cls.rename_model_to_experiments_command.replace(old_model_name, model_name)
+
+    # 上傳wav到server
     @classmethod
     def upload_wav_to_server(cls):
         try:
@@ -25,9 +40,24 @@ class SSH:
             message = '上傳失敗，請檢查連線'
         return message
 
+    # 秀出model列表
+    @classmethod
+    def show_model_list(cls):
+        try:
+            t = paramiko.Transport((cls.host, cls.port))
+            t.connect(username=cls.user, password=cls.password)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            message = '連接成功'
+            return message, sorted([name for name in sftp.listdir(cls.show_model_list_command) if name.startswith('model')], key=lambda x: int(x.replace('model_', '')))
+        except:
+            traceback.print_exc()
+            message = '連接失敗'
+            return message, []
+
     @classmethod
     def convert_wav(cls):
         try:
+            cls.rename_model_name()
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=cls.host, port=cls.port, username=cls.user, password=cls.password)
@@ -40,9 +70,35 @@ class SSH:
             traceback.print_exc()
         return message
 
+    # 將server的model資料夾改為experiments名稱
+    @classmethod
+    def rename_model_name(cls):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=cls.host, port=cls.port, username=cls.user, password=cls.password)
+        stdin, stdout, stderr = ssh.exec_command(cls.rename_model_to_experiments_command)
+        ssh.close()
+        if (stderr.read().decode('utf-8') != ''):
+            raise Exception
+
+    # 將server的experiments資料夾改為原來model_name
+    @classmethod
+    def rename_experiments_name(cls):
+        old_model_name = cls.rename_experiments_to_model_command.split(' ')[-1]
+        cls.rename_experiments_to_model_command = cls.rename_experiments_to_model_command.replace(old_model_name, cls.model_name)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=cls.host, port=cls.port, username=cls.user, password=cls.password)
+        stdin, stdout, stderr = ssh.exec_command(cls.rename_experiments_to_model_command)
+        ssh.close()
+        if (stderr.read().decode('utf-8') != ''):
+            raise Exception
+
+    # 從server上下載wav檔
     @classmethod
     def download_wav_from_server(cls):
         try:
+            cls.rename_experiments_name()
             cls.sftp_download_file()
             message = '下載成功'
         except:
@@ -50,6 +106,7 @@ class SSH:
             message = '下載失敗，請檢查連線'
         return message
 
+    # local上傳檔案至server
     @classmethod
     def sftp_upload_file(cls):
         t = paramiko.Transport((cls.host, cls.port))
@@ -61,6 +118,7 @@ class SSH:
              sftp.put(cls.local_source_wav_dir_path + '/' + wav, cls.server_source_wav_dir_path + '/' + wav)
         t.close()
 
+    # server下載檔案至local
     @classmethod
     def sftp_download_file(cls):
         t = paramiko.Transport((cls.host, cls.port))
@@ -72,18 +130,18 @@ class SSH:
             sftp.get(file_path, cls.local_target_wav_dir_path + f)
         t.close()
 
+    # 在上傳音檔前，先將server上的所有舊的音檔先刪除
     @classmethod
     def remove_server_files_from_dir(cls, sftp):
         server_files = sftp.listdir(cls.server_source_wav_dir_path)
         for f in server_files:
             file_path = posixpath.join(cls.server_source_wav_dir_path, f)
-            print(file_path)
             sftp.remove(file_path)
         server_files = sftp.listdir(cls.server_target_wav_dir_path)
         for f in server_files:
             file_path = posixpath.join(cls.server_target_wav_dir_path, f)
-            print(file_path)
             sftp.remove(file_path)
+
 
 
 
--- voice_conversion_ssh.py
+++ voice_conversion_ssh.py
@@ -13,6 +13,22 @@ class AppWindow(QMainWindow):
         self.ui.pb_upload.clicked.connect(self.upload_click)
         self.ui.pb_convert.clicked.connect(self.convert_click)
         self.ui.pb_download.clicked.connect(self.download_click)
+        self.ui.pb_show_model.clicked.connect(self.show_model_list)
+        self.ui.listWidget.itemClicked.connect(self.model_click)
+
+    def show_model_list(self):
+        self.ui.listWidget.clear()
+        message, model_list = SSH.show_model_list()
+        if message == '連接失敗':
+            QMessageBox.information(self,
+                                    '>_<',
+                                    message + ', 無法取得model列表',
+                                    QMessageBox.Yes)
+        else:
+            self.ui.listWidget.addItems(model_list)
+
+    def model_click(self, item):
+        SSH.init_path(item.text())
 
     def upload_click(self):
         message = SSH.upload_wav_to_server()
@@ -44,3 +60,4 @@ sys.exit(app.exec_())
 # SSH.upload_wav_to_server()
 # SSH.convert_wav()
 # SSH.download_wav_from_server()
+# SSH.show_model_list()
